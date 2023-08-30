1425d4ded7a206b8bd542a5374d93cf2e14a3e95
Azeem <livelyazeem@gmail.com>
2023-08-30 23:31:55 +0500

    changed some controllers for frontend

---
+++ controllers/logoutController.js
@@ -0,0 +1,25 @@
+const User = require("../model/User");
+
+const handleLogout = async (req, res) => {
+  // On client, also delete the accessToken
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204); // No content to send back
+  const refreshToken = cookies.jwt;
+
+  // Is refreshToken in db?
+  const foundUser = await User.findOne({ refreshToken }).exec();
+  if (!foundUser) {
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+    return res.sendStatus(204);
+  }
+
+  // Delete refreshToken in db
+  foundUser.refreshToken = "";
+  const result = await foundUser.save();
+  console.log(result);
+
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true }); // set cookies to secure: true - in production so that they are only served over https
+  res.sendStatus(204);
+};
+
+module.exports = { handleLogout };
